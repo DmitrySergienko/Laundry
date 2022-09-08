faef9c7e6b36b8de8f8ae4d8d355a855a6999e7c
DmitrySergienko <dmitrisergienko@gmail.com>
2022-09-08 15:57:13 +0400

    Splash screen

--- app/src/main/java/com/laundry/presentation/MainActivity.kt
+++ app/src/main/java/com/laundry/presentation/MainActivity.kt
@@ -1,14 +1,53 @@
 package com.laundry.presentation
 
+import android.animation.ObjectAnimator
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.laundry.R
+import com.laundry.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.concurrent.Executors
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //Splash Screen launch
+        splashScreenLaunch()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun splashScreenLaunch() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val splashScreen = installSplashScreen()
+
+            splashScreen.setKeepOnScreenCondition { true }
+            Executors.newSingleThreadExecutor().execute {
+                Thread.sleep(0)
+                splashScreen.setKeepOnScreenCondition { false }
+            }
+            splashScreen.setOnExitAnimationListener { splashScreenProvider ->
+                ObjectAnimator.ofFloat(
+                    splashScreenProvider.view,
+                    View.TRANSLATION_Y,
+                    0f, -splashScreenProvider.view.height.toFloat()
+                ).apply {
+                    duration = 500
+                    interpolator = AnticipateInterpolator() //with acceleration
+                    doOnEnd { splashScreenProvider.remove() }
+                }.start()
+            }
+        }
     }
 }
