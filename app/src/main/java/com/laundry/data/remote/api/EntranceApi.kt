5a8c1726c44a0ccce9a57653df655cf966e9b30e
DmitrySergienko <dmitrisergienko@gmail.com>
2022-09-09 11:28:19 +0400

    entrance entities and EntranceApi

--- app/src/main/java/com/laundry/data/remote/api/EntranceApi.kt
+++ app/src/main/java/com/laundry/data/remote/api/EntranceApi.kt
@@ -1,7 +1,18 @@
 package com.laundry.data.remote.api
 
+import com.laundry.domain.entity.LoginRequest
+import com.laundry.domain.entity.LoginResponse
+import com.laundry.domain.entity.Registration
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 interface EntranceApi {
 
+    @POST("pls/apex/pmstest/MobileApp/login")
+    suspend fun pushLogin(@Body post: LoginRequest): LoginResponse
 
+    @POST("pls/apex/pmstest/MobileApp/registration")
+    suspend fun pushRegistration(@Body post: Registration): Response<Void>
 
 }
---
+++ app/src/main/java/com/laundry/domain/entity/LoginResponse.kt
@@ -0,0 +1,25 @@
+package com.laundry.domain.entity
+
+import com.google.gson.annotations.SerializedName
+
+data class LoginResponse (
+
+    @field:SerializedName("EMAIL")
+    val pEMAIL: String? = null,
+
+    @field:SerializedName("LOGIN_FLAG")
+    val pLOGIN_FLAG: String? = null,
+
+
+    @field:SerializedName("PASSWORD")
+    val pPASSWORD: String? = null,
+
+    @field:SerializedName("LAST_NAME")
+    val pLASTNAME: String? = null,
+
+    @field:SerializedName("PHONE")
+    val pPHONE: String? = null,
+
+    @field:SerializedName("FIRST_NAME")
+    val pFIRSTNAME: String? = null
+)
