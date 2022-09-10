b4b54ccf5f347d75a2a54c6c9b5674ba8aa2b1e2
DmitrySergienko <dmitrisergienko@gmail.com>
2022-09-10 23:21:53 +0400

    Signup layout

--- app/src/main/java/com/laundry/presentation/entrance/SignupFragment.kt
+++ app/src/main/java/com/laundry/presentation/entrance/SignupFragment.kt
@@ -24,43 +24,43 @@ class SignupFragment
         super.onViewCreated(view, savedInstanceState)
 
         //back button
-        binding.backButton.setOnClickListener { it1->
-            Navigation.findNavController(it1).navigate(R.id.action_signupFragment_to_loginFragment)
-        }
+//        binding.backButton.setOnClickListener { it1->
+//            Navigation.findNavController(it1).navigate(R.id.action_signupFragment_to_loginFragment)
+//        }
 
         //enter button
-        binding.enterButton.setOnClickListener {
-            val email = binding.emailEditText.text.toString()
-            val password = binding.passwordEditText.text.toString()
-            val lastName = binding.lastNameEditText.text.toString()
-            val firstName = binding.firstNameEditText.text.toString()
-            val phone = binding.phoneEditText.text.toString()
-
-            if (email.isBlank() || password.isBlank() || lastName.isBlank()
-                || firstName.isBlank() || phone.isBlank()
-            ) {
-                Toast.makeText(requireContext(), "PLEASE COMPLETE ALL FIELDS", Toast.LENGTH_SHORT)
-                    .show()
-            } else {
-                val myPost = Registration(email, password, lastName, phone, firstName)
-                viewModel.pushRegistration(myPost)
-
-                lifecycleScope.launchWhenStarted {
-                    viewModel.signup.collectLatest {
-                        if (it.isSuccessful) {
-
-                            view.let { it1 ->
-                                Navigation.findNavController(it1)
-                                    .navigate(R.id.action_signupFragment_to_loginFragment)
-                            }
-
-                        } else {
-                            Log.d("VVV", it.body().toString())
-                        }
-                    }
-                }
-            }
-        }
+//        binding.enterButton.setOnClickListener {
+//            val email = binding.emailEditText.text.toString()
+//            val password = binding.passwordEditText.text.toString()
+//            val lastName = binding.lastNameEditText.text.toString()
+//            val firstName = binding.firstNameEditText.text.toString()
+//            val phone = binding.phoneEditText.text.toString()
+//
+//            if (email.isBlank() || password.isBlank() || lastName.isBlank()
+//                || firstName.isBlank() || phone.isBlank()
+//            ) {
+//                Toast.makeText(requireContext(), "PLEASE COMPLETE ALL FIELDS", Toast.LENGTH_SHORT)
+//                    .show()
+//            } else {
+//                val myPost = Registration(email, password, lastName, phone, firstName)
+//                viewModel.pushRegistration(myPost)
+//
+//                lifecycleScope.launchWhenStarted {
+//                    viewModel.signup.collectLatest {
+//                        if (it.isSuccessful) {
+//
+//                            view.let { it1 ->
+//                                Navigation.findNavController(it1)
+//                                    .navigate(R.id.action_signupFragment_to_loginFragment)
+//                            }
+//
+//                        } else {
+//                            Log.d("VVV", it.body().toString())
+//                        }
+//                    }
+//                }
+//            }
+//        }
 
 
     }
