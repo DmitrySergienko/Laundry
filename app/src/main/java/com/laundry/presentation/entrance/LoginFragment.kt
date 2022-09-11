cea9f8f743f1515b045b3d4e5042aa3fa4355dfc
DmitrySergienko <dmitrisergienko@gmail.com>
2022-09-11 22:41:50 +0400

    navigation to home screen

--- app/src/main/java/com/laundry/presentation/entrance/LoginFragment.kt
+++ app/src/main/java/com/laundry/presentation/entrance/LoginFragment.kt
@@ -4,68 +4,63 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
 import com.laundry.R
 import com.laundry.databinding.FragmentLoginBinding
-import com.laundry.presentation.BaseFragment
+import com.laundry.domain.entity.LoginRequest
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
 @AndroidEntryPoint
-class LoginFragment
-    : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::inflate){
-
+class LoginFragment: Fragment(){
+    private var _binding: FragmentLoginBinding? = null
+    private val binding: FragmentLoginBinding
+        get() = _binding!!
 
     private val viewModel: LoginViewModel by viewModels()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
-
-
-        //go to signup screen
-        binding.serviceProviderButton.setOnClickListener { it1 ->
-
-            Navigation.findNavController(it1)
-                .navigate(R.id.action_loginFragment_to_homeServiceProviderFragment2)
-
-        }
-
-
-
-
-//        binding.loginButton.setOnClickListener {
-//            val email = binding.editTextTextEmailAddress.text.toString()
-//            val password = binding.editTextTextPassword.text.toString()
-//
-//            val myPost = LoginRequest(email, password)
-//            viewModel.pushLogin(myPost)
-//
-//            lifecycleScope.launchWhenStarted {
-//                viewModel.login.collectLatest {
-//                    if (it.pLOGIN_FLAG == "Y") {
-//
-//                        view.let { it1 ->
-//                            Navigation.findNavController(it1)
-//                                .navigate(R.id.action_loginFragment_to_homeClientFragment)
-//
-//                        }
-//                    } else {
-//                        Toast.makeText(
-//                            requireContext(),
-//                            "INCORRECT EMAIL OR PASSWORD",
-//                            Toast.LENGTH_SHORT
-//                        ).show()
-//                    }
-//                }
-//            }
-//        }
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
 
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.loginButton.setOnClickListener {
+            val email = binding.editTextTextEmailAddress.text.toString()
+            val password = binding.editTextTextPassword.text.toString()
+
+            val myPost = LoginRequest(email, password)
+            viewModel.pushLogin(myPost)
+
+            lifecycleScope.launchWhenStarted {
+                viewModel.login.collectLatest {
+                    if (it.pLOGIN_FLAG == "Y") {
+
+                        view.let { it1 ->
+                            Navigation.findNavController(it1).navigate(R.id.action_mainFragment_to_homeClientFragment)
+                        }
+                    } else {
+                        Toast.makeText(
+                            requireContext(),
+                            "INCORRECT EMAIL OR PASSWORD",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
+        }
+
+        }
 
 
     companion object {
