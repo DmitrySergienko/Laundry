e36a7990e4d4e9d2551b1bdc8aa06079286c9a7f
DmitrySergienko <dmitrisergienko@gmail.com>
2022-09-11 22:53:57 +0400

    navigation to main fragment

--- app/src/main/java/com/laundry/presentation/client/HomeClientFragment.kt
+++ app/src/main/java/com/laundry/presentation/client/HomeClientFragment.kt
@@ -1,5 +1,9 @@
 package com.laundry.presentation.client
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.Navigation
+import com.laundry.R
 import com.laundry.databinding.FragmentHomeClientBinding
 import com.laundry.presentation.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -8,6 +12,24 @@ import dagger.hilt.android.AndroidEntryPoint
 class HomeClientFragment
     : BaseFragment<FragmentHomeClientBinding>(FragmentHomeClientBinding::inflate) {
 
+    private var increment = 0
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        binding.buttonPlus.setOnClickListener {
+            increment++
+            binding.textView12.text =increment.toString()
+        }
+        binding.buttonMinus.setOnClickListener {
+            increment--
+            binding.textView12.text =increment.toString()
+        }
+
+        binding.imageView2.setOnClickListener {
+            view.let { it1 ->
+                Navigation.findNavController(it1).navigate(R.id.action_homeClientFragment_to_mainFragment)
+            }
+        }
+    }
 }
