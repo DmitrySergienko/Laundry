10c3570975c2ca24467cc1a884f95eb673091098
DmitrySergienko <dmitrisergienko@gmail.com>
2022-09-12 08:47:58 +0400

    image view adjustment

--- app/src/main/java/com/laundry/presentation/entrance/MainFragment.kt
+++ app/src/main/java/com/laundry/presentation/entrance/MainFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import com.google.android.material.tabs.TabLayoutMediator
+import com.laundry.R
 import com.laundry.databinding.FragmentMainBinding
 import com.laundry.presentation.entrance.viewpager.VpAdapter
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,8 +20,9 @@ class MainFragment : Fragment() {
         SignupFragment.newInstance()
     )
     private val tList = listOf(
-        "Hours",
-        "Days"
+        "LOGIN",
+        "SIGNUP"
+
     )
 
     private lateinit var binding: FragmentMainBinding
